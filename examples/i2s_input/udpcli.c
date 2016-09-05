763eeaa45cac51c43640640d37c712179d20f9d6
Kazumoto Kojima <kkojima@drone.co.jp>
2016-09-05 13:16:33 +0900

    examples/i2s_input: Add an i2s input example using ADMP441 i2s mic.

---
+++ examples/i2s_input/udpcli.c
@@ -0,0 +1,107 @@
+/* A udp client.
+   Send IMU/compass samples and recieve pmw data.
+ */
+
+#include "espressif/esp_common.h"
+#include "esp/uart.h"
+
+#include <string.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include <semphr.h>
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+#include "ssid_config.h"
+
+#define ECHO_SERVER "10.253.253.41"
+#define PORT 6000
+
+extern void i2s_init(void);
+extern void i2s_task(void *pvParameters);
+
+static int sockfd = -1;
+
+static void udp_task(void *pvParameters)
+{
+    struct sockaddr_in saddr;
+    struct sockaddr_in caddr;
+    int rtn;
+
+    printf("UDP client task starting...\r\n");
+
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    if(s < 0) {
+        printf("... Failed to allocate socket.\r\n");
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        return;
+    }
+
+    printf("... allocated socket\r\n");
+
+    memset((char *) &caddr, 0, sizeof(caddr));
+    caddr.sin_family = AF_INET;
+    caddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    caddr.sin_port = htons(PORT);
+
+    memset((char *) &saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = inet_addr (ECHO_SERVER);
+    saddr.sin_port = htons(PORT);
+
+    rtn = bind (s, (struct sockaddr *)&caddr, sizeof(caddr));
+    if(rtn < 0) {
+        printf("... Failed to bind socket.\r\n");
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        close (s);
+        return;
+    }
+    rtn = connect(s, (struct sockaddr *) &saddr, sizeof(saddr));
+    if (rtn < 0) {
+        printf("... Failed to connect socket.\r\n");
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        close (s);
+        return;
+    }
+
+    while (1) {
+        sockfd = s;
+        vTaskSuspend(NULL);
+    }
+}
+
+void user_init(void)
+{
+    uart_set_baud(0, 115200);
+
+    i2s_init();
+
+    // Just some information
+    printf("\n");
+    printf("SDK version : %s\n", sdk_system_get_sdk_version());
+    printf("GIT version : %s\n", GITSHORTREV);
+
+    struct sdk_station_config config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASS,
+    };
+
+    // required to call wifi_set_opmode before station_set_config
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&config);
+
+    // start udp task
+    xTaskCreate(udp_task, (int8_t *)"udp_task", 256, NULL, 3, NULL);
+
+    while (sockfd < 0) {
+        vTaskDelay(100 / portTICK_RATE_MS);
+    }
+
+    // Create i2s task
+    xTaskCreate(i2s_task, (int8_t *)"i2s_task", 512, (void*)sockfd, 2, NULL);
+}
